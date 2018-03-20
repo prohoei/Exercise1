87c6842b23c1f8d9b5d96416f03398dc6d578bb2
Alexander Høi <heyman2525@hotmail.com>
2018-03-20 12:02:09 +0100

    Created project

---
+++ Start/FunFlacts/AllFlags.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlagData;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FunFlacts
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AllFlags : ContentPage
+    {
+        public AllFlags()
+        {
+            InitializeComponent();
+
+            BindingContext = DependencyService.Get<FunFlactsViewModel>();
+        }
+    }
+}
